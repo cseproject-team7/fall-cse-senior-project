fb3e6f2694f27a1f7272db2d065b4a2f849ec8ea
Icycoal <88856841+Icycoal@users.noreply.github.com>
2025-10-22 22:00:06 -0400

    Change static file serving directory to 'public'

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve static files from React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // API routes
 app.use('/api', apiRoutes);
